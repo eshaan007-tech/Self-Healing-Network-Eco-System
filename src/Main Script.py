806b2b2234243fd086609d7dab47643cd14be7d8
eshaan007-tech <eshaanuddin777@gmail.com>
2026-04-21 06:32:13 +0000

    Final Chagnes Done

---
+++ src/Main Script.py
@@ -0,0 +1,44 @@
+import time
+import ansible_runner
+import ollama
+
+try:
+    import requests
+    has_requests = True
+except ImportError:
+    import urllib.request
+    import urllib.error
+    has_requests = False
+
+# Monitoring Script
+URL = "http://127.0.0.1:5000"
+
+
+def check_service():
+    try:
+        if has_requests:
+            response = requests.get(URL, timeout=2)
+            status = response.status_code
+        else:
+            req = urllib.request.Request(URL)
+            with urllib.request.urlopen(req, timeout=2) as resp:
+                status = resp.getcode()
+
+        if status == 200:
+            print("IS UP")
+        else:
+            print("NOT UP")
+    except Exception:
+        print("Service Error")
+        status = None
+    return status
+
+
+while True:
+    status = check_service()
+    if status is None or status != 200:
+        ansible_runner.run(private_data_dir='Brain/project/', playbook='logs_checking.yaml')
+    time.sleep(5)
+
+
+
